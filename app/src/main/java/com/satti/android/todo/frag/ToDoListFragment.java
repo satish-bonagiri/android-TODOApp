939c958e70d21c49c13719d8db0b0cc4ad1120cb
satish <satish_mtech@yahoo.com>
2016-07-30 16:13:10 +0530

    enabling option menu in fragment
    enabling option menu in fragment

--- app/src/main/java/com/satti/android/todo/frag/ToDoListFragment.java
+++ app/src/main/java/com/satti/android/todo/frag/ToDoListFragment.java
@@ -95,6 +95,7 @@ public class ToDoListFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
